bfd4bb8fe99342cbab31b866a3f57a76af17a676
unknown <swapnilkasar95@gmail.com>
2019-11-23 13:31:39 +0530

    oops-ts : classes and objects

---
+++ Typescript/device.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var oop_ts_1 = require("./oop-ts");
+var rm = new oop_ts_1.Remote();
+var cl = new oop_ts_1.Cell();
+var btRemote = new oop_ts_1.BTRemote();
+var rem = {
+    range: 10,
+    rfBw: 2,
+    signal: true,
+    turnOff: function () { },
+    turnOn: function () { }
+};
